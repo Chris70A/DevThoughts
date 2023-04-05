bb9dce9a06a616ddec91c2b234abffa8c657f609
Christian Plasencia <113798773+Chris70A@users.noreply.github.com>
2023-04-05 08:08:28 -0700

    made connections between models

--- models/index.js
+++ models/index.js
@@ -2,6 +2,24 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
+Post.belongsTo(User, {                                // POST  belongs to Users
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {                              // Posts has many Comments
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {                            // Comment belongs to Users
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+
+
+
 module.exports = {
   User,
   Comment,
